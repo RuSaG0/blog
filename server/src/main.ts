2a13929a20b1096edde66aed3dc4ea9b3ce3bcd4
RuSaG0 <mirzoev-ruslan-2000@mail.ru>
2023-05-28 00:26:45 +0400

    req from front

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: true, // Allow requests from all origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
+
+  // Enable CORS
+  app.enableCors(corsOptions);
+
+
   await app.listen(process.env.PORT || 5000);
 }
+
 bootstrap();
