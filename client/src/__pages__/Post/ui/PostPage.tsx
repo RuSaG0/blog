8581cecaea3a6b50fe44e5f95d965be07eea8716
RuSaG0 <mirzoev-ruslan-2000@mail.ru>
2023-06-04 01:37:17 +0400

    render

--- client/src/__pages__/Post/ui/PostPage.tsx
+++ client/src/__pages__/Post/ui/PostPage.tsx
@@ -1,39 +1,28 @@
 import cls from './PostPage.module.scss'
 import {useRouter} from 'next/router';
 import axiosInstance from '../../../shared/api/axiosInstance';
-import {useEffect, useState} from 'react';
-import EditorJsParser from 'editorjs-html';
-import edjsParser from 'editorjs-parser'
-
-const parseEditorJs = async (data) => {
-    const parser = new EditorJsParser();
-    const html = await parser.parse(data);
-    return html;
-};
+import {useEffect, useRef, useState} from 'react';
+import Editor from '@editorjs/editorjs';
+import Output from 'editorjs-react-renderer';
 
 interface PostPageProps {
     className?: string;
 }
 
 const PostPage = ({className}: PostPageProps) => {
-    const parser = new edjsParser();
+    const editorRef = useRef(null);
     const router = useRouter();
     const { id } = router.query;
 
     const [post, setPost] = useState(null);
-    const [content, setContent] = useState('');
+    const [content, setContent] = useState(null);
 
-    // Assuming you have a function to fetch the post based on the ID
     const fetchPost = async () => {
         try {
             const response = await axiosInstance.get(`/posts/${id}`);
             setPost(response.data)
-            console.info(1, response.data.content)
-            const data = JSON.parse(await response.data.content)
-            console.info(2, data)
-            const html = await parseEditorJs(data);
-            console.info(2, html)
-            setContent(html)
+            const content = JSON.parse(response.data.content)
+            setContent(content);
         } catch (error) {
             console.error('Error:', error);
         }
@@ -51,8 +40,9 @@ const PostPage = ({className}: PostPageProps) => {
             {post && <>
                 <div>
                     <p>{post.header}</p>
-                    <div dangerouslySetInnerHTML={{ __html: content }} />
-
+                  <Output
+                    data={content}
+                  />
                 </div>
             </>}
         </div>
