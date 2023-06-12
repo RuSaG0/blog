42dbc9a998873d1d249abd0cfb651431e5a9a6bf
RuSaG0 <mirzoev-ruslan-2000@mail.ru>
2023-06-13 00:04:42 +0400

    add user crud

--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -1,1 +1,10 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+    @IsEmail({}, { message: 'Invalid email format' })
+    @IsNotEmpty({ message: 'Email should not be empty' })
+    email: string;
+
+    @IsNotEmpty({ message: 'Password should not be empty' })
+    password: string;
+}
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import {User, UserRole} from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+      @InjectRepository(User)
+      private readonly userRepository: Repository<User>)
+  {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const user = this.userRepository.create(createUserDto);
+    Object.assign(user, {role: UserRole.USER});
+    return this.userRepository.save(user);
   }
 
   findAll() {
